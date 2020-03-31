c790d0ce0ab9b658b4cd3791a59d8e49bef052c9
xiejiajun <xiejiajun@zeppelin.com>
2020-04-01 01:19:24 +0800

    Resource对象进一步分析

--- zeppelin-interpreter/src/test/java/org/apache/zeppelin/resource/ResourceTest.java
+++ zeppelin-interpreter/src/test/java/org/apache/zeppelin/resource/ResourceTest.java
@@ -32,4 +32,18 @@ public class ResourceTest {
     ByteBuffer buffer = Resource.serializeObject("hello");
     assertEquals("hello", Resource.deserializeObject(buffer));
   }
+
+
+  @Test
+  public void testInvokeMethod_shouldAbleToInvokeMethodWithTypeInference(){
+    // TODO ..由此可见 Resource的invokeMethod就是用来反射执行资源对象（执行结果、状态等都是资源对象）的某些方法的，跟普通的反射工具类没啥区别
+    Resource r = new Resource(null, new ResourceId("pool1", "name1"), "object");
+    assertEquals("ect", r.invokeMethod("substring",new Class[]{int.class}, new Object[]{3}));
+    assertEquals("obj", r.invokeMethod("substring",new Class[]{int.class,int.class}, new Object[]{0,3}));
+    assertEquals(true, r.invokeMethod("startsWith",new Class[]{String.class}, new Object[]{"obj"}));
+
+    assertEquals(2, r.invokeMethod("indexOf",new Class[]{int.class}, new Object[]{'j'}));
+    assertEquals(4, r.invokeMethod("indexOf",new Class[]{String.class,int.class}, new Object[]{"ct",3}));
+
+  }
 }
